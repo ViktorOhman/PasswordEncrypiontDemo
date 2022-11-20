10461bc47ed4ccdb89bd52a2e3d0643b9fbb4c47
Vikto <89028157+ViktorOhman@users.noreply.github.com>
2022-11-20 09:15:26 +0200

    Update PasswordSet.py

--- PasswordEncryptionDemo/PasswordSet.py
+++ PasswordEncryptionDemo/PasswordSet.py
@@ -3,17 +3,17 @@ from cryptography.fernet import Fernet
 import time
 
 key = Fernet.generate_key() #Generate a key
-with open("PasswordKey.key", "wb") as keyfile:
+with open("PasswordKey.key", "wb") as keyfile: #Writte the key to a File
     keyfile.write(key)
-
+#New function
 def encrypt(check):
     while check:
         #Ask for the password using pyautogui
         password = pyautogui.password(text='Input Your desired password', title='Password Encrypter', mask='*')
-        paasword_c = pyautogui.confirm(text='Is this your Password? \n\n' + password, title='???', buttons=['YES', 'NO'])
+        paasword_c = pyautogui.confirm(text='Is this your Password? \n\n' + password, title='???', buttons=['YES', 'NO']) #This Line returns YES or NO
 
         if paasword_c == 'YES':
-            password = bytes(password, 'utf-8') #For encryption the string must be tranformed to bytes
+            password = bytes(password, 'utf-8') #For encryption the string must be tranformed into bytes
             password_e = Fernet(key).encrypt(password) # Encrypt the password usig our key
             #Write the password to a file
             with open("thepassword.txt", "wb") as passwordfile:
